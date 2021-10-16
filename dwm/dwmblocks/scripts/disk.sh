e9989851ed97424d7f6e7682ac8059026d0bf363
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2021-10-16 22:28:26 +0530

    dwm changes

--- dwm/dwmblocks/scripts/disk.sh
+++ dwm/dwmblocks/scripts/disk.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-df | awk 'FNR == 4 {print $4/1000000 "GB"}';
-
+case $BLOCK_BUTTON in
+    1)mem="$(df | awk 'FNR == 4 {print $4/1000000 "GB"}')" ;;
+    3)notify-send "Memmory Consumers 
+        $(ps axch -o cmd:15,%mem --sort=-%mem | head) ";;
+esac
+#mem="$(df | awk 'FNR == 4 {print $4/1000000 "GB"}')"
+echo -e "$mem"
